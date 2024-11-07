3310ddc826e4e23a9fb8bd4269728014425f073a
bradley.tran <bradley.tran@wynnlasvegas.com>
2024-11-06 20:39:10 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conferenceTickets: is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of : %v tickets and remaining tickets are: %v\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var bookings []string
+
+	var firstName string
+	var lastName string
+	var userEmail string
+	var userTickets uint
+	// ask user for their name
+
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email")
+	fmt.Scan(&userEmail)
+
+	fmt.Println("How many tickets you want to book?")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets // update remaining tickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, userEmail)
+	fmt.Printf("Remaining tickets are %v for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("Bookings so far:%v", bookings)
+}
